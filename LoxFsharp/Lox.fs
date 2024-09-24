d0986107d382cbcead3737b029d7b2e45dd45cc2
curtiscao <cz188b@foxmail.com>
2024-09-25 01:04:16 +0800

    feat: init commit

---
+++ LoxFsharp/Lox.fs
@@ -0,0 +1,28 @@
+﻿module Lox.Main
+
+open System.IO
+
+let run text = printfn $"{text}"
+
+let runFile path = path |> File.ReadAllText |> run
+
+[<TailCall>]
+let rec runPrompt () =
+    printf "> "
+
+    match stdin.ReadLine() with
+    | null -> ()
+    | line ->
+        run line
+        runPrompt ()
+
+[<EntryPoint>]
+let main args =
+    match args with
+    | [||] -> runPrompt ()
+    | [| file |] -> runFile file
+    | _ ->
+        eprintfn "Usage: lox [script]"
+        exit 64
+
+    0
