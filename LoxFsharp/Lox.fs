1b6d428385b9a46ff88d4e0135aa5ee005bf34c8
curtiscao <cz188b@foxmail.com>
2024-09-28 09:49:03 +0800

    feat: refactor

--- LoxFsharp/Lox.fs
+++ LoxFsharp/Lox.fs
@@ -1,19 +1,14 @@
 ﻿module rec LoxFsharp.Lox
 
-open System
 open System.IO
+open LoxFsharp.Utils
+open LoxFsharp.Scanner
 
 type Lox() =
-    static let mutable hadError = false
-
-    static member report line where message =
-        printfn $"[line {line}] Error{where}: {message}"
-        hadError <- true
-
-    static member error line message = Lox.report line "" message
+    let reporter = ErrReporter()
 
     member this.run text =
-        let scanner = Scanner(text)
+        let scanner = Scanner(text, reporter)
         let tokens = scanner.scanTokens ()
 
         for token in tokens do
@@ -22,7 +17,7 @@ type Lox() =
     member this.runFile path =
         File.ReadAllText path |> this.run
 
-        if hadError then
+        if reporter.hadError then
             exit 65
 
     [<TailCall>]
@@ -33,212 +28,8 @@ type Lox() =
         | null -> ()
         | line ->
             this.run line
-            hadError <- false
+            reporter.hadError <- false
             this.runPrompt ()
-
-type Scanner(source: string) =
-    static let keywords =
-        Map
-            [ "and", AND
-              "class", CLASS
-              "else", ELSE
-              "false", FALSE
-              "for", FOR
-              "fun", FUN
-              "if", IF
-              "nil", NIL
-              "or", OR
-              "print", PRINT
-              "return", RETURN
-              "super", SUPER
-              "this", THIS
-              "true", TRUE
-              "var", VAR
-              "while", WHILE ]
-
-
-    let tokens = ResizeArray<Token>()
-    let mutable start = 0
-    let mutable current = 0
-    let mutable line = 1
-
-    let isDigit c = '0' <= c && c <= '9'
-
-    let isAlpha c =
-        ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || c = '_'
-
-    let isAlphaDigit c = (isAlpha c) || (isDigit c)
-
-    let isAtEnd () = current >= source.Length
-
-    let advance () =
-        let c = source[current]
-        current <- current + 1
-        c
-
-    let match1 expect =
-        if isAtEnd () || (source[current] <> expect) then
-            false
-        else
-            current <- current + 1
-            true
-
-    let peek () =
-        if isAtEnd () then char 0 else source[current]
-
-    let peekNext () =
-        if current + 1 >= source.Length then
-            char 0
-        else
-            source[current + 1]
-
-    let addToken (typ: TokenType) (literal: Object) =
-        tokens.Add
-            { typ = typ
-              lexme = source.Substring(start, current - start)
-              literal = literal
-              line = line }
-
-    let addTokenSimple (typ: TokenType) = addToken typ null
-
-    let scanString () =
-        while peek () <> '"' && not (isAtEnd ()) do
-            if peek () = '\n' then
-                line <- line + 1
-
-            advance () |> ignore
-
-        if isAtEnd () then
-            Lox.error line "Unterminated string."
-        else
-            advance () |> ignore
-            let value = source.Substring((start + 1), (current - 1) - (start + 1))
-            addToken STRING value
-
-    let scanNumber () =
-        while isDigit (peek ()) do
-            advance () |> ignore
-
-        if peek () = '.' && isDigit (peekNext ()) then
-            advance () |> ignore
-
-        while isDigit (peek ()) do
-            advance () |> ignore
-
-        addToken NUMBER (Double.Parse(source.Substring(start, current - start)))
-
-    let scanIdentifier () =
-        while isAlphaDigit (peek ()) do
-            advance () |> ignore
-
-        let text = source.Substring(start, current - start)
-
-        if keywords.ContainsKey(text) then
-            addTokenSimple (keywords[text])
-        else
-            addTokenSimple IDENTIFIER
-
-    let scanToken () =
-        let c = advance ()
-
-        match c with
-        | '(' -> addTokenSimple LEFT_PAREN
-        | ')' -> addTokenSimple RIGHT_PAREN
-        | '{' -> addTokenSimple LEFT_BRACE
-        | '}' -> addTokenSimple RIGHT_BRACE
-        | ',' -> addTokenSimple COMMA
-        | '.' -> addTokenSimple DOT
-        | '-' -> addTokenSimple MINUS
-        | '+' -> addTokenSimple PLUS
-        | ';' -> addTokenSimple SEMICOLON
-        | '*' -> addTokenSimple STAR
-        | '!' -> addTokenSimple (if match1 '=' then BANG_EQUAL else BANG)
-        | '=' -> addTokenSimple (if match1 '=' then EQUAL_EQUAL else EQUAL)
-        | '<' -> addTokenSimple (if match1 '=' then LESS_EQUAL else LESS)
-        | '>' -> addTokenSimple (if match1 '=' then GREATER_EQUAL else GREATER)
-        | '/' ->
-            if match1 '/' then
-                while (peek () <> '\n') && not (isAtEnd ()) do
-                    advance () |> ignore
-            else
-                addTokenSimple SLASH
-        | ' '
-        | '\r'
-        | '\t' -> ()
-        | '\n' -> line <- line + 1
-        | '"' -> scanString ()
-        | c when isDigit c -> scanNumber ()
-        | c when isAlpha c -> scanIdentifier ()
-        | _ -> Lox.error line $"Unexpect character {c}."
-
-    member this.scanTokens() =
-        while not (isAtEnd ()) do
-            start <- current
-            scanToken ()
-
-        tokens.Add
-            { typ = EOF
-              lexme = ""
-              literal = null
-              line = line }
-
-        tokens
-
-[<Struct>]
-type Token =
-    { typ: TokenType
-      lexme: string
-      literal: Object
-      line: int }
-
-    override this.ToString() =
-        $"{this.typ} %s{this.lexme} %A{this.literal}"
-
-type TokenType =
-    // Single-character tokens.
-    | LEFT_PAREN
-    | RIGHT_PAREN
-    | LEFT_BRACE
-    | RIGHT_BRACE
-    | COMMA
-    | DOT
-    | MINUS
-    | PLUS
-    | SEMICOLON
-    | SLASH
-    | STAR
-    // One or two character tokens.
-    | BANG
-    | BANG_EQUAL
-    | EQUAL
-    | EQUAL_EQUAL
-    | GREATER
-    | GREATER_EQUAL
-    | LESS
-    | LESS_EQUAL
-    // Literals.
-    | IDENTIFIER
-    | STRING
-    | NUMBER
-    // Keywords.
-    | AND
-    | CLASS
-    | ELSE
-    | FALSE
-    | FUN
-    | FOR
-    | IF
-    | NIL
-    | OR
-    | PRINT
-    | RETURN
-    | SUPER
-    | THIS
-    | TRUE
-    | VAR
-    | WHILE
-    | EOF
-
 
 [<EntryPoint>]
 let main args =
